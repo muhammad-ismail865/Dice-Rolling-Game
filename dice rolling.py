8a47ae9cce946d95a40799a45fe58d695bc97158
Muhammad ismail <ismailjanjua865@gmail.com>
2026-04-25 12:17:04 +0500

    Update dice rolling.py

--- dice rolling.py
+++ dice rolling.py
@@ -2,8 +2,8 @@ import random
 while True:
     dice=input("Roll the dice (y/n):").lower()
     if(dice=="y"):
-        die1=random.randint(1,10)
-        die2=random.randint(1,10)
+        die1=random.randint(1,6)
+        die2=random.randint(1,6)
         print(f"{die1,die2}")
     elif(dice=="n"):
         print("Thanks for Playing")
